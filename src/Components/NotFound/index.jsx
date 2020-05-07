3943eaa9a2272b45b192c224561d21e20ed7d985
David Sommers <davidcsommers@gmail.com>
2020-05-06 23:58:24 -0700

    Implement 404 page component

--- src/Components/NotFound/index.jsx
+++ src/Components/NotFound/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './NotFound.scss';
+import WhiteboardLg from '../WhiteboardLg';
+import WhiteboardSm from '../WhiteboardSm';
 
 function NotFound() {
   return (
-    <div>
-      NotFound
-    </div>
+    <WhiteboardLg className="NotFound">
+      <WhiteboardSm className="NotFound-wb-sm">
+        <h1>
+          Welcome to CS 404:
+        </h1>
+        <h1>
+          Page Not Found
+        </h1>
+      </WhiteboardSm>
+    </WhiteboardLg>
   )
 }
 
